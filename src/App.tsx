8d778676fd272750d5513cea9417e7fa2580c6ab
emanuelefavero <emafaux89@gmail.com>
2024-03-05 14:48:12 +0100

    add i18next main code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css'
+import './i18n.ts'
+import { useTranslation } from 'react-i18next'
 
 function App() {
+  const { t, i18n } = useTranslation()
+
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language)
+  }
+
   return (
     <>
-      <h1>Hello</h1>
+      <h1>{t('welcome')}</h1>
+      <button onClick={() => changeLanguage('en')}>EN</button>
+      <button onClick={() => changeLanguage('it')}>IT</button>
     </>
   )
 }
